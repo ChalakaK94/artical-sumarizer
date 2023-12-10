3c601f923dfe06b4f250c478a82a4be153ceff01
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-10 21:56:47 +0530

    implement header text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,21 @@
+
+import {logo} from '../assets'
 export function Hero() {
     return (
-        <></>
+        <>
+            <header className='w-full  flex justify-center items-center flex-col'>
+                <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                    <img src={logo} alt="logo" className='w-28 object-contain'/>
+                    <button onClick={()=>window.open('','_blank')}
+                            className='black_btn'> GitHub</button>
+                </nav>
+
+                <h1 className='head_text'>Summarize Article</h1>
+                <h2 className='desc'>
+                    Simplify with your reading with Summarize, an Open source article summarizer
+                    that transform length articles into clear and concise summaries
+                </h2>
+            </header>
+        </>
     )
 }
