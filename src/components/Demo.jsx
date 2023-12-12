b22bbc5a264c42982e5260e2678b393573598f11
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-12 16:21:27 +0530

    implement summarize history links

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -45,8 +45,15 @@ export function Demo() {
                     <button type='submit' className='submit_btn peer-focus: border-gray-700 peer-focus:text-gray-700'>Submit</button>
                 </form>
 
-                <div>
-
+                <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.map((item,index)=>(
+                        <div key={`link-${index}`} onClick={()=>setArticle(item)} className='link_card'>
+                            <div className='copy_btn'>
+                                <img src={copy} alt=" " className='w-[40%] h-[40%] object-contain' />
+                            </div>
+                            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
 
