3217dfa31f6692af73646a761aeb1c30315904da
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-12 16:47:55 +0530

    implement result area

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -56,6 +56,28 @@ export function Demo() {
                     ))}
                 </div>
             </div>
+            <div className="my-10 max-w-full flex justify-center items-center">
+                {isFetching ? (
+                    <img src={loader} alt="loader" className='w-10 h-10 object-contain'/>
+                ) : error ? (
+                        <p className='font-inter font-bold text-black text-center'>Something went wrong...
+                            <br/>
+                            <span className='font-satoshi font-normal text-gray-700'>{error?.data?.error}</span>
+                        </p>
+                ): (
+                    article.summary &&(
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                                Article <span className='blue_gradient'>Summary</span>
+                            </h2>
+
+                            <div className='summary_box'>
+                                <p className='font-inter font-medium'>{article.summary}</p>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
 
         </section>
     )
